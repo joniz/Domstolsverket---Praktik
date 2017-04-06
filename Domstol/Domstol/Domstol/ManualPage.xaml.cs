4cb20c0465f5cc5bfe896794c05c3a5ee815ac62
joniz <jaej96@gmail.com>
2017-04-06 12:51:21 +0200

    NU är det gött

---
+++ Domstol/Domstol/Domstol/ManualPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Domstol
+{
+    public partial class ManualPage : ContentPage
+    {
+        public ManualPage()
+        {
+            InitializeComponent();
+            itemlist.ItemsSource = Item.items;
+        }
+        void OnItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                var selected = e.SelectedItem as Item;
+                //Navigation.PushAsync(new InformationPage(selected));
+
+            }
+        }
+
+    }
+}
