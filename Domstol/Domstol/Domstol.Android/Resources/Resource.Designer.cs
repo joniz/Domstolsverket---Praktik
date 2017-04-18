3a17bcbad9e419c932a66c251a78145fbc257e52
joniz <jaej96@gmail.com>
2017-04-18 15:52:35 +0200

    Göttigt

--- Domstol/Domstol/Domstol.Android/Resources/Resource.Designer.cs
+++ Domstol/Domstol/Domstol.Android/Resources/Resource.Designer.cs
@@ -2209,11 +2209,17 @@ namespace Domstol.Droid
 			// aapt resource value: 0x7f0200a3
 			public const int mr_ic_play_light = 2130837667;
 			
-			// aapt resource value: 0x7f0200a5
-			public const int notification_template_icon_bg = 2130837669;
+			// aapt resource value: 0x7f0200a7
+			public const int notification_template_icon_bg = 2130837671;
 			
 			// aapt resource value: 0x7f0200a4
-			public const int videoPic2 = 2130837668;
+			public const int Sal = 2130837668;
+			
+			// aapt resource value: 0x7f0200a5
+			public const int Start = 2130837669;
+			
+			// aapt resource value: 0x7f0200a6
+			public const int videoPic2 = 2130837670;
 			
 			static Drawable()
 			{
